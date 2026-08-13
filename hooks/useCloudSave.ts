fadd5d3b6da0c23eea80eccb11383b40a6a87cc9
Codespace <codespace@users.noreply.github.com>
2026-08-13 20:46:38 +0000

    help admin

--- hooks/useCloudSave.ts
+++ hooks/useCloudSave.ts
@@ -171,7 +171,7 @@ export function useCloudSave(userId: string | null) {
       if (typeof data.pixelCoins === 'number') patch.pixelCoins = data.pixelCoins;
       if (typeof data.nekoGems   === 'number') patch.nekoGems   = data.nekoGems;
       if (typeof data.bossCrowns === 'number') patch.bossCrowns = data.bossCrowns;
-      useGameStore.setState(patch as Parameters<typeof useGameStore.setState>[0]);
+      useGameStore.setState(patch as unknown as Parameters<typeof useGameStore.setState>[0]);
     });
     return unsub;
   }, [userId]);
