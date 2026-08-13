826b539b9652cc265bd16906f5fb1ca811be3fac
Codespace <codespace@users.noreply.github.com>
2026-08-13 21:23:25 +0000

    resolve code

--- lib/game/giftCodes.ts
+++ lib/game/giftCodes.ts
@@ -26,9 +26,11 @@ export const GIFT_CODES: GiftCodeDef[] = [
   //{ code: 'NEKOZ-RICH-4F7H',  pixelCoins: 10_000_000 },
 
   // ── Code Arthur Leywin ────────────────────────────────────────────
-  {
-    code:         'UWU-BAKA-GV2',
-    characters:   ['ouchuu'],
+  /*{
+    code:         'ARTHUR-LEYWIN-GV2',
+    pixelCoins:   700_000_000,
+    characters:   ['arthur_leywin', 'arthur_leywin'],
+    gems:1200,
   },
   {
     code:         'CODE-SORRY-INES',
